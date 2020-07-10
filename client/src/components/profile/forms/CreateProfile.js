becce1f6381d05e927ca29940eb2ed98750ac4f0
samdi18 <41270492+samdi18@users.noreply.github.com>
2020-07-10 17:24:11 +0600

    link to edit

--- client/src/components/profile/forms/CreateProfile.js
+++ client/src/components/profile/forms/CreateProfile.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-import { withRouter } from "react-router-dom";
-import ProgrammerProfileForm from "./ProgrammerProfileForm";
-import HirerProfileForm from "./HirerProfileForm";
-import { connect } from "react-redux";
-import { createProfile, getMyProfile } from "../../../actions/profile";
+import { withRouter } from 'react-router-dom';
+import ProgrammerProfileForm from './ProgrammerProfileForm';
+import HirerProfileForm from './HirerProfileForm';
+import { connect } from 'react-redux';
+import { createProfile, getMyProfile } from '../../../actions/profile';
 
 const CreateProfile = ({
   createProfile,
@@ -13,25 +13,29 @@ const CreateProfile = ({
   profile: { profile, loading },
   history,
 }) => {
-  const [role, setRole] = useState("Programmer");
+  const [role, setRole] = useState('Programmer');
   const [formData, setFormData] = useState({});
 
   // useEffect(() => {
   //   if (!profile) getMyProfile(); // !null = true
 
-  // if (!loading && profile) {
-  //   const profileData = { ...formData };
-  //   for (const key in profile) {
-  //     if (key in profileData) profileData[key] = profile[key];
+  //   if (!loading && profile) {
+  //     const profileData = { ...formData };
+  //     for (const key in profile) {
+  //       if (key in profileData) profileData[key] = profile[key];
+  //     }
+  //     for (const key in profile.social) {
+  //       if (key in profileData) profileData[key] = profile.social[key];
+  //     }
+  //     if (Array.isArray(profileData.skills))
+  //       profileData.skills = profileData.skills.join(', ');
+  //     setFormData(profileData);
   //   }
-  //   for (const key in profile.social) {
-  //     if (key in profileData) profileData[key] = profile.social[key];
-  //   }
-  //   if (Array.isArray(profileData.skills))
-  //     profileData.skills = profileData.skills.join(', ');
-  //   setFormData(profileData);
-  // }
   // }, [loading, getMyProfile, profile]);
+
+  useEffect(() => {
+    if (!profile) getMyProfile();
+  });
 
   const handleRole = (e) => {
     setRole(e.target.value);
@@ -44,30 +48,30 @@ const CreateProfile = ({
   };
 
   return (
-    <div className="form-flex">
-      <div className="form-wrapper card wrapper">
-        <h4 className="form-header">Create Profile</h4>
+    <div className='form-flex'>
+      <div className='form-wrapper card wrapper'>
+        <h4 className='form-header'>Create Profile</h4>
 
-        <form className="profile-form" onSubmit={handleSubmit}>
-          <div className="form-element">
-            <label htmlFor="role">* Role</label>
-            <select name="role" defaultValue={role} onChange={handleRole}>
-              <option value="Hirer">Hirer</option>
-              <option value="Programmer">Programmer</option>
+        <form className='profile-form' onSubmit={handleSubmit}>
+          <div className='form-element'>
+            <label htmlFor='role'>* Role</label>
+            <select name='role' defaultValue={role} onChange={handleRole}>
+              <option value='Hirer'>Hirer</option>
+              <option value='Programmer'>Programmer</option>
             </select>
           </div>
 
-          {role === "Programmer" ? (
+          {role === 'Programmer' ? (
             <ProgrammerProfileForm setFormData={setFormData} />
           ) : (
             <HirerProfileForm setFormData={setFormData} />
           )}
 
-          <button className="submit-btn">
+          <button className='submit-btn'>
             <img
-              src={require("../../../images/arrow.svg")}
-              alt=""
-              className="arrow-img"
+              src={require('../../../images/arrow.svg')}
+              alt=''
+              className='arrow-img'
             />
           </button>
         </form>

e059df07979b5dc62e87f647d18464dffb0c7fa5
samdi18 <41270492+samdi18@users.noreply.github.com>
2020-07-26 17:44:53 +0600

    thread component

--- client/src/components/thread/Thread.js
+++ client/src/components/thread/Thread.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import ThreadItem from '../threads/ThreadItem';
+import CommentForm from '../thread/CommentForm';
+import CommentItem from '../thread/CommentItem';
+import { getThread } from '../../actions/thread';
 
 const Thread = (props) => {
   return <div>Post</div>;
 };
 
-Thread.propTypes = {};
+Thread.propTypes = {
+  getThread: PropTypes.func.isRequired,
+  thread: PropTypes.object.isRequired,
+};
 
-export default Thread;
+const mapStatetoProps = (state) => ({
+  thread: state.thread,
+});
+
+export default connect(mapStatetoProps, { getThread })(Thread);
